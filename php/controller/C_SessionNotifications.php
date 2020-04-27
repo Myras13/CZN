ea3d577947add37e274d5400b1482f7fce5210b5
Michał Marciniak <majckel12@gmail.com>
2020-04-27 11:29:45 +0200

    Poprawienie kodu

--- php/controller/C_SessionNotifications.php
+++ php/controller/C_SessionNotifications.php
@@ -2,7 +2,10 @@
 
     require_once(dirname(__DIR__).'/class/SessionNotifications.php');
 
-    if(!SessionNotifications::isAlive()){
+    $reflector = new ReflectionClass('SessionNotifications');
+    $instance = $reflector->newInstanceWithoutConstructor();
+
+    if(!$instance->isAlive()){
 
         $context = null;
 
@@ -12,7 +15,7 @@
         $context['mode'] = $_SESSION["mode"];
         $context['content'] = $_SESSION["content"];
         $context['title'] = $_SESSION["title"];
-        SessionNotifications::destroy();
+        $instance->destroy();
 
     }
 
--- php/controller/C_logOut.php
+++ php/controller/C_logOut.php
@@ -2,7 +2,9 @@
 
     require_once(dirname(__DIR__).'/class/SessionUser.php');
 
-    SessionUser::destroy();
+    $reflector = new ReflectionClass('SessionUser');
+    $instance = $reflector->newInstanceWithoutConstructor();
+    $instance->destroy();
 
     $host  = $_SERVER['HTTP_HOST'];
     header("Location: http://$host/CZN");
