3351b8fa4f72252e81e5e9c7ed07b70c320e8d47
Michał Marciniak <majckel12@gmail.com>
2020-05-10 18:05:00 +0200

    compare return bool, examine return 3 opcje

--- php/class/ValidatePassword.php
+++ php/class/ValidatePassword.php
@@ -7,24 +7,23 @@
         public function compare(string $pass1, string $pass2): bool{
 
             if(strcasecmp($pass1, $pass2) != 0)
-                throw new ValidateDataUserException('Podane hasła są różne.',1);
+                return false;
                
             return true;
 
         }
 
-        public function examine(string $pass, int $countChar): bool{ 
+        public function examine(string $pass, int $countChar): int{ 
 
             if(strlen($pass) < $countChar)
-                throw new ValidateDataUserException('Hasło musi mieć co najmniej '.$countChar.' znaków.',2);
+                return -1;
             
             $reg = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?!.* )(?=.*[!@#\$%\^&\*])/";    
         
             if(!preg_match($reg, $pass))
-                throw new ValidateDataUserException('Hasło musi mieć co najmniej:</br>- 1 wielki znak,</br>- 1 mały znak, </br>- 1 liczbę,</br>- Nie mieć białych znaków(spacja, tabulacja).',3);
-        
-        
-            return true;
+                return 0;
+                
+            return 1;
 
         }
 
