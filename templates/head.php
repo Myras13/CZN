1c34da366da6e72aa4cfc330ac816844212f14ff
Michal Turbanski <michalturbanski@gmail.com>
2020-05-12 21:33:28 +0200

    BugFix

--- templates/head.php
+++ templates/head.php
@@ -3,8 +3,5 @@
 <link href="style.css" rel="stylesheet" type="text/css"/>
 <link href="https://fonts.googleapis.com/css?family=Lobster&display=swap&subset=latin-ext" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Montserrat:700&display=swap&subset=latin-ext" rel="stylesheet">
-<<<<<<< HEAD
 <script type="text/javascript" src="https://platform-api.sharethis.com/js/sharethis.js#property=5eb9a0cd7525e90012616de9&product=inline-share-buttons" async="async"></script>
-=======
 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
->>>>>>> michal_t
