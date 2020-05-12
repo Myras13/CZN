713c92ce165b43c1d7e99f2ba51b834ad0e6d022
Erlya <kszymek33@gmail.com>
2020-05-12 19:47:13 +0200

    share

--- templates/head.php
+++ templates/head.php
@@ -3,3 +3,4 @@
 <link href="style.css" rel="stylesheet" type="text/css"/>
 <link href="https://fonts.googleapis.com/css?family=Lobster&display=swap&subset=latin-ext" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Montserrat:700&display=swap&subset=latin-ext" rel="stylesheet">
+<script type="text/javascript" src="https://platform-api.sharethis.com/js/sharethis.js#property=5eb9a0cd7525e90012616de9&product=inline-share-buttons" async="async"></script>
