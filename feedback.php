f01e492da5e4803b5c236227d07fac2445e81303
Michał Marciniak <majckel12@gmail.com>
2020-05-17 02:25:23 +0200

    move part code to V_FeedbackForm.php

--- feedback.php
+++ feedback.php
@@ -3,8 +3,6 @@
     if (session_status() == PHP_SESSION_NONE)
         session_start();
 
-    $email = (isset($_SESSION["email"]))? $_SESSION["email"]: null;
-
 ?>
 
 <!DOCTYPE HTML>
@@ -24,20 +22,7 @@
 
             <div id="feedback">
                 <form action="php/controller/C_FeedbackSend.php" method="post">
-
-                        <input name="nick" type="hidden" value="<?php if(isset($_SESSION['nick'])) echo $_SESSION['nick']; ?>" required/><br/><br/>
-
-                        <label for="title">Tytuł</label><br/>
-                        <input id="title" name="title" type="text" required/><br/><br/>
-
-                        <label for="email">Email</label><br/>
-                        <input id="email" name="email" type="email" value="<?php if($email != null) echo $email; ?>" required/><br/><br/>
-
-                        <?php require_once('php/view/V_FeedbackOption.php');?><br/><br/>
-
-                        <label for="message">Treść wiadomości</label><br/>
-                        <textarea id="message" name="message" placeholder="Napisz wiadomość" required></textarea><br/>
-                        <input type="submit" value="Wyślij wiadomość"/>
+                    <?php require_once('php/view/V_FeedbackForm.php');?>
                 </form>
             </div>
 
