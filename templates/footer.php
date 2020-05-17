ecbd5b328e81fdb2034e2ab64476e39904654049
Michał Marciniak <majckel12@gmail.com>
2020-05-17 02:29:32 +0200

    Add link to feedback.php

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,5 @@
 <div id="footer">
     <div class="sharethis-inline-share-buttons"></div>
     Coś z niczego &copy; All rights reserved
+    <a href='feedback.php'> Skontaktuj się z nami </a>
 </div>
