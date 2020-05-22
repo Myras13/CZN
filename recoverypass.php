a725bd819c289e479bcc6551eff9317e9e4d1c49
Michał Marciniak <majckel12@gmail.com>
2020-05-22 15:35:08 +0200

    Utworzenie recoverypass.php

---
+++ recoverypass.php
@@ -0,0 +1,30 @@
+<?php
+
+    if (session_status() == PHP_SESSION_NONE)
+        session_start();
+ 
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <?php include("templates/head.php") ?>
+    </head>
+
+    <body>
+    <?php require_once('php/view/V_SessionNotifications.php');?>
+        <div id="container">
+            
+            <?php include("templates/logo.php") ?>
+            <?php include("templates/topbar.php") ?>
+
+            <div id="content_index">
+                <?php require_once('php/view/V_RecoveryPassword.php');?>
+            </div>
+
+            <?php include("templates/footer.php") ?>
+
+        </div>
+    </body>
+</html>
