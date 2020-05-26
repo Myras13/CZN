30814010045ebccfeff3fb9e9bd7132efe76af87
Michał Marciniak <majckel12@gmail.com>
2020-05-26 20:05:56 +0200

    Utworzenie ManagementPage

---
+++ php/class/ManagementPage.php
@@ -0,0 +1,28 @@
+<?php
+
+    require_once(dirname(__DIR__).'/core/ConnectDatabase.php'); 
+
+    abstract class ManagementPage{
+
+        protected $pdo;
+
+        public function __construct(){
+
+            try{
+
+                $this->pdo = new ConnectDatabase();
+    
+            }catch(PDOException $e){
+                
+                $host  = $_SERVER['HTTP_HOST']; 
+                $errorInfo = new SessionNotifications('error', 'Błąd krytyczny',"Nie udało połączyć się z bazą danych.");
+                $errorInfo->create();            
+                header("Location: http://$host/CZN");
+                
+            }
+
+        }
+
+    }
+
+?>
