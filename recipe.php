b59494340cf5a2f9ba7f590556332ddec3025f35
Michał Marciniak <majckel12@gmail.com>
2020-05-26 20:05:43 +0200

    Utworzenie recipe.php

---
+++ recipe.php
@@ -0,0 +1,28 @@
+<?php
+
+    if (session_status() == PHP_SESSION_NONE)
+        session_start();
+ 
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <?php include("templates/head.php") ?>
+    </head>
+
+    <body>
+        <?php require_once('php/view/V_SessionNotifications.php');?>
+        <div id="container">
+            
+            <?php include("templates/logo.php") ?>
+            <?php include("templates/topbar.php") ?>
+
+            <div id="content_index">
+                <?php require_once('php/view/V_Recipe.php');?>
+            <?php include("templates/footer.php") ?>
+
+        </div>
+    </body>
+</html>
