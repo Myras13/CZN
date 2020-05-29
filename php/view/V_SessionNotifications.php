058ca7cd97abbc1f273dbbb9323152be75d33be2
Erlya <kszymek33@gmail.com>
2020-05-29 21:55:51 +0200

    Close button

--- php/view/V_SessionNotifications.php
+++ php/view/V_SessionNotifications.php
@@ -7,8 +7,9 @@
 <?php if (!empty($context)): ?>
 
     <div class="<?php echo $context['mode'] ?>">
-        <div id="title"> <h2><?php echo $context['title'] ?> </h2></div>
-        <div id="content"> <p><?php echo $context['content'] ?> </p></div>
+        <input type="button" name="close" value="X" onclick="parentNode.remove()"/>   
+        <div id="title"> <h2><?php echo $context['title'] ?></h2></div>
+        <div id="content"> <p><?php echo $context['content'] ?> </p></div>   
     </div>
 
 <?php endif; ?>
