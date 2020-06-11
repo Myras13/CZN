b4f0d28f358e5f733399116f75c14d2efb3cfe32
Michał Marciniak <majckel12@gmail.com>
2020-06-11 20:06:21 +0200

    Dodawanie systemu dodawania przepisów

--- php/controller/C_AddRecipe.php
+++ php/controller/C_AddRecipe.php
@@ -1,11 +1,17 @@
 <?php
- 
+
+    if (session_status() == PHP_SESSION_NONE)
+        session_start();
+
     require_once(dirname(__DIR__).'/class/SessionNotifications.php');
+    require_once(dirname(__DIR__).'/model/M_AddRecipe.php');
+    require_once(dirname(__DIR__).'/model/M_AddIngredients.php');
+    
 
     $data['name_of_recipe'] = (isset($_POST['name_of_recipe']) && $_POST['name_of_recipe'] != '') ? htmlspecialchars($_POST['name_of_recipe']) : null;
     $data['text_of_recipe'] = (isset($_POST['text_of_recipe']) && $_POST['name_of_recipe'] != '') ? htmlspecialchars($_POST['text_of_recipe']) : null;
     $data['type_recipe'] = (isset($_POST['type_recipe']) && $_POST['type_recipe'] != '' && intval($_POST['type_recipe']) != 0) ? htmlspecialchars($_POST['type_recipe']) : null;
-
+    
     $host  = $_SERVER['HTTP_HOST'];
 
     foreach($data as $value){
@@ -28,6 +34,15 @@
     }
 
 
-    var_dump($_POST)
+    $newRecipe = new M_AddRecipe($data['name_of_recipe'], $data['text_of_recipe'], $data['type_recipe']);
+    $newRecipe->setUser($_SESSION['id']);
+    $idNewRecipe = $newRecipe->add();
+    
+    $newIngredients = new M_AddIngredients($idNewRecipe, $postValues);
+    $newIngredients->add();
+
+    $succesInfo = new SessionNotifications('success', 'Udało się', "Przepis został dodany");
+    $succesInfo->create();            
+    header("Location: http://$host/CZN/recipe.php?id=$idNewRecipe");
 
 ?>
