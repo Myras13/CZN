364eb226606ec54f376c22f237c4757b250e7abf
Michał Marciniak <majckel12@gmail.com>
2020-06-11 20:07:36 +0200

    System dodawania przepisów

--- php/model/M_AddRecipe.php
+++ php/model/M_AddRecipe.php
@@ -1,6 +1,7 @@
 <?php
 
     require_once(dirname(__DIR__).'/class/ManagementRecipe.php'); 
+    require_once(dirname(__DIR__).'/class/SessionNotifications.php'); 
 
     class M_AddRecipe extends ManagementRecipe{
 
@@ -17,15 +18,49 @@
 
         }
 
-        public function setUser(string $nick){
+        public function __destruct(){
 
-            $this->user = nick;
+            $this->pdo->disconnect();
+            
+        }
+
+        public function setUser(int $id){
+
+            $this->user = $id;
 
         }
 
         public function add(){
+            
+            $sthPDO = $this->pdo->getPDO();
+            $sth = $sthPDO->prepare("INSERT INTO recipe (id_user, id_type, title, content, date) VALUES (?, ?, ?, ?, ?);");
+            $sth->bindValue(':idUser', $this->user, PDO::PARAM_INT);
+            $sth->bindValue(':idType', $this->type, PDO::PARAM_INT);
+            $sth->bindValue(':title', $this->title, PDO::PARAM_STR);
+            $sth->bindValue(':content', $this->text, PDO::PARAM_STR);
+            $sth->bindValue(':date', $this->date, PDO::PARAM_STR);
 
+            try {
 
+                $sthPDO->beginTransaction();
+                $sth->execute([$this->user, $this->type, $this->title, $this->text, $this->date]);
+                $lastId = $sthPDO->lastInsertId();
+                $sthPDO->commit();
+
+            } catch(PDOExecption $e) {
+
+                $sthPDO->rollback();
+                $host  = $_SERVER['HTTP_HOST']; 
+                $errorInfo = new SessionNotifications('error', 'Błąd krytyczny',"Nie udało się dodać przepisu.");
+                $errorInfo->create();            
+                header("Location: http://$host/CZN/add_recipe.php");
+                return;
+
+            }finally{
+
+                return intval($lastId);
+
+            }
 
         }
 
