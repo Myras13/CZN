e18cb889e54da56318861cc3c30be136086eb9ba
Michał Marciniak <majckel12@gmail.com>
2020-06-12 23:36:07 +0200

    Ustawienie konkretnej wartosci w option

--- php/view/V_TypeRecipeInAdd.php
+++ php/view/V_TypeRecipeInAdd.php
@@ -4,11 +4,18 @@
         require_once(dirname(__DIR__).'/controller/C_ShowTypeRecipe.php');
         $max = sizeof($routelink);
         for ($i = 0; $i < $max; $i++):
+            if(isset($recipe['id_type'])):
     ?>
 
-    <option value=<?php echo $routelink[$i]['id']."" ?>><?php echo $routelink[$i]['category'] ?></option>
+                <option value=<?php echo $routelink[$i]['id']."" ?> <?php if(isset($recipe['id_type']) && $recipe['id_type'] == $routelink[$i]['id']) echo "selected"; else ''; ?>><?php echo $routelink[$i]['category'] ?></option>
 
     <?php
+            else:
+    ?>
+                <option value=<?php echo $routelink[$i]['id']."" ?>><?php echo $routelink[$i]['category'] ?></option>
+    <?php
+
+            endif;
         endfor;
     ?>
 
